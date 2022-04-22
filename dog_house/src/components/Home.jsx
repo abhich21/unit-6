f749a5e8e938934d543439b8a87bdfe21b676aae
abhich21 <96258754+abhich21@users.noreply.github.com>
2022-04-23 01:01:27 +0530

    added sorting

--- dog_house/src/components/Home.jsx
+++ dog_house/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { addPetHouse } from "../redux/action"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 
 export const Home = () => {
+	const [sortData,setSortData]=useState({parameter:"",value:""})
+
     const dispatch = useDispatch()
     const data = useSelector((store) => store.pethouse)
     useEffect(() => {
@@ -19,7 +21,8 @@ export const Home = () => {
     }, [])
 
     const Table = styled.table`
-	margin:auto
+	margin:auto;
+	padding-top:20px
 	`
 	const Th = styled.th`
 	border: 2px solid red;
@@ -30,9 +33,15 @@ export const Home = () => {
 	border: 2px solid red;
 	padding-right: 50px;
 	`
-
+	const handleSort = (parameter,value) => {
+		setSortData({parameter,value})
+	}
     return (
-        <>
+		<>
+			<button onClick={()=> handleSort("cost",1)} style={{width:"100px" ,height:"30px", fontSize:"11px"}}>cost low to high</button>
+			<button onClick={()=> handleSort("cost",-1)} style={{width:"100px" ,height:"30px", fontSize:"11px"}}>cost high to low</button>
+			<button onClick={()=> handleSort("rating",1)} style={{width:"100px" ,height:"30px", fontSize:"11px"}}>rating low to high</button>
+			<button onClick={()=> handleSort("rating",-1)} style={{width:"100px" ,height:"30px", fontSize:"11px"}}>rating high to low</button>
            <Table >
 		<thead>
 			<tr >
@@ -47,7 +56,22 @@ export const Home = () => {
 			</tr>
 		</thead>
 		<tbody >
-			{data.map((el) => {
+			{data
+			.sort((a,b)=>{
+				if(sortData.parameter == "cost" && sortData.value== 1){
+						return a.cost-b.cost;
+				}
+				else if(sortData.parameter == "cost" && sortData.value== -1){
+						return b.cost-a.cost;
+				}
+				else if(sortData.parameter == "rating" && sortData.value== 1){
+						return a.rating-b.rating;
+				}
+				else if(sortData.parameter == "rating" && sortData.value== -1){
+						return b.rating-a.rating;
+				}
+			})
+			.map((el) => {
 				
 				return (
 					<tr key={el.id} >
